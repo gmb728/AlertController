78cc951e9308bee5a03e4ff14e8a31adda0d74fd
Chang Sophia <you@example.com>
2018-11-27 14:29:20 +0800

    initial

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var keyInAn = 0
+ 
+    @IBOutlet weak var blackJackTextField: UITextField!
+   
+    @IBOutlet weak var blackjackButton: UIButton!
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+//empty
+        if blackJackTextField.text?.isEmpty == true {
+            let controller = UIAlertController(title: "It's your turn", message: "Don't lose the chance\n to be a millionaire!", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style:.default, handler: nil)
+            controller.addAction(okAction)
+            self.present(controller, animated: true, completion: nil)
+            
+        }
 
+        }
+        
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        blackjackButton.layer.masksToBounds = false
+        self.blackjackButton.layer.shadowOffset = CGSize(width:-10,height:10)
+        self.blackjackButton.layer.shadowColor = UIColor.darkGray.cgColor
+        self.blackjackButton.layer.shadowOpacity = 0.8
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
